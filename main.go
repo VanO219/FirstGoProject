6b3802d26f6e7b87e9c250396be5b16847f62964
Ivan Sinyavsky <ivan-tg@mail.ru>
2021-01-24 01:34:15 +0300

    myFirstWebServer

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, GO!")
+	s := http.Server{
+		Addr:    "localhost:80",
+		Handler: http.HandlerFunc(handleHello),
+	}
+
+	fmt.Println(s.ListenAndServe())
+}
+
+func handleHello(w http.ResponseWriter, r *http.Request) {
+	_, err := w.Write([]byte("Hello HTTP"))
+	if err != nil {
+		fmt.Printf("ALARM %s", err)
+	}
 }
